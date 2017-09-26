95fa57c79826b4a027db8728376297b0c045b62b
Stefan Huber <junktogo@web.de>
2017-09-26 12:10:06 +0200

    Closes #3

--- reportsScript.sql
+++ reportsScript.sql
@@ -1,5 +1,5 @@
 /* Sum up invalid reasons */
-CREATE VIEW invalidReasons AS (
+CREATE OR REPLACE VIEW invalidReasons AS (
     SELECT invalid_reason, sum(count) AS occurrence
     FROM profilescops
     GROUP BY invalid_reason
@@ -7,20 +7,20 @@ CREATE VIEW invalidReasons AS (
 );
 
 /* Durations per run */
-CREATE VIEW durationsPerRun AS (
+CREATE OR REPLACE VIEW durationsPerRun AS (
     SELECT run.id, run.command, run.project_name, run.run_group, EXTRACT(MICROSECONDS FROM run."end" - run."begin") AS duration
     FROM run INNER JOIN rungroup ON run.run_group = rungroup.id
 );
 
 /* Durations per project */
-CREATE VIEW durationsPerProject AS (
+CREATE OR REPLACE VIEW durationsPerProject AS (
     SELECT project_name, sum(duration) AS duration
     FROM durationsperrun
     GROUP BY project_name
 );
 
 /* Sum of durations of SCoPs */
-CREATE VIEW durationsScopsPerProject AS (
+CREATE OR REPLACE VIEW durationsScopsPerProject AS (
     SELECT project_name, sum(regions.duration) AS duration
     FROM regions LEFT OUTER JOIN run ON run.id = regions.run_id
     INNER JOIN rungroup ON run.run_group = rungroup.id
@@ -28,7 +28,7 @@ CREATE VIEW durationsScopsPerProject AS (
 );
 
 /* Ratio of SCoPs per project */
-CREATE VIEW ratios AS (
+CREATE OR REPLACE VIEW ratios AS (
     SELECT dp.project_name, ds.duration / dp.duration AS ratio
     FROM durationsperproject AS dp FULL OUTER JOIN durationsscopsperproject AS ds ON dp.project_name = ds.project_name
 );
